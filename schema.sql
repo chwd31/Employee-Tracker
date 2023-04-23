5836dfb5995980721d7bf5493c65db60f83f8965
chwd31 <chwdwile@aol.com>
2023-04-23 16:26:04 -0500

    fix seeding issues

--- schema.sql
+++ schema.sql
@@ -1,27 +1,33 @@
+
 DROP DATABASE IF EXISTS emptracker_db;
 CREATE DATABASE emptracker_db;
 
 USE emptracker_db;
 
+-- Create department table with an auto-incrementing 'id' column
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name VARCHAR(30)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
+-- Create role table with an auto-incrementing 'id' column
 CREATE TABLE role (
-  id INT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
+-- Create employee table with an auto-incrementing 'id' column
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
+
+
